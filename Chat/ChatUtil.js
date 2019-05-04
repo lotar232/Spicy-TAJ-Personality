477c20540ad85189dcecbf583a69cff6847f6e3c
lotar232 <36368222+lotar232@users.noreply.github.com>
2019-05-04 12:18:42 -0700

    Add files via upload

--- Chat/ChatUtil.js
+++ Chat/ChatUtil.js
@@ -31,3 +31,30 @@ function sendSystemMessage(message) {
 
     sendCustomMessage(text);
 }
+
+
+function sendArbMessage(textName,message, wait, ImagePath) {
+ 
+    sendName = new javafx.scene.text.Text(textName);
+    sendName.setFill(javafx.scene.paint.Color.ROYALBLUE);
+    sendName.setFont(javafx.scene.text.Font.font(null, javafx.scene.text.FontWeight.BOLD, 14));
+
+
+    message = replaceVocab(message);
+    text = new javafx.scene.text.Text(message);
+    text.setFill(javafx.scene.paint.Color.ROYALBLUE);
+    text.setFont(javafx.scene.text.Font.font(null, javafx.scene.text.FontWeight.MEDIUM, 13));
+
+    sendCustomMessage(sendName, text);
+	    //Show image
+  //  if(ImagePath !== undefined) {
+  //      showImage(ImagePath);
+   // }
+
+    if(wait === undefined ) {
+        sleep(1000 + message.length * 50, "MILLISECONDS");
+    }else
+		{sleep(wait*1000, "MILLISECONDS");
+		}
+
+}
