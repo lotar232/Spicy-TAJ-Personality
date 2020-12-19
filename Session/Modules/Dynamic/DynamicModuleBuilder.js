2d82142b08c4088b67e51e4cacbb6717d2098f32
lotar232 <36368222+lotar232@users.noreply.github.com>
2020-12-18 21:59:42 -0800

    Add files via upload

---
+++ Session/Modules/Dynamic/DynamicModuleBuilder.js
@@ -0,0 +1,83 @@
+//Categories
+const MODULE_CBT = 0;
+const MODULE_ANAL = 1;
+const MODULE_BALL_TORTURE = 2;
+const MODULE_COCK_TORTURE = 3;
+const MODULE_UNKNOWN = 4;
+const MODULE_BLOWJOB = 5;
+const MODULE_STROKING = 6;
+const MODULE_WATCH_VIDEO = 7;
+const MODULE_HUMILIATION = 8;
+const MODULE_TASKS = 9;
+const MODULE_CORNER_TIME = 10;
+const MODULE_LINE_WRITING = 11;
+const MODULE_ENEMA = 12;
+const MODULE_SPANKING = 13;
+const MODULE_PEG = 0;
+
+const TRANSITION_NO = 0;
+const TRANSITION_ALWAYS = 1;
+const TRANSITION_POSSIBLE = 2;
+
+
+const PREVIOUS_MODULE_CATEGORIES = new java.util.ArrayList();
+
+const DYNAMIC_MODULES = [];
+
+
+function registerCurrentModuleCategory(category) {
+    PREVIOUS_MODULE_CATEGORIES.add(category);
+}
+
+function hasPreviousModuleHadCategory(category) {
+    return PREVIOUS_MODULE_CATEGORIES.contains(category);
+}
+
+function clearPreviousModuleHistory() {
+    PREVIOUS_MODULE_CATEGORIES.clear();
+}
+
+
+function registerDynamicModule(module) {
+		sendDebugMessage("in registerDynamicModule");	
+    module.moduleId = getCurrentScriptName().toLowerCase();
+    module.getChance = function() {
+        let chance = this.frequency;
+
+        if (MODULE_HISTORY.isInTodaysHistory(this.moduleId)) {
+            chance *= this.repeatPerSession/10;
+        }
+
+        let minModulesSinceRun = 3;
+
+        if (MODULE_HISTORY.isInHistory(this.moduleId)) {
+            //Check whether not enough modules have passed since last time we ran this module
+            if (MODULE_HISTORY.getModulesSinceHistory(this.moduleId) < minModulesSinceRun) {
+                let tries = getVar('findModuleTries');
+                /*if (tries < maxTries / 2) {
+                    //Try to run from same category
+                    runModuleCategory(category);
+                    return false;
+                } else if (tries < maxTries) {
+                    //Try to find a different module
+                    run("Session/Modules/DecideModule.js");
+                    return false;
+                }*/
+                //Else
+                //We tried too often so we gotta let this pass
+
+                return 0;
+            }
+        }
+
+        return chance;
+    };
+
+
+    module.preRun = function() {
+        MODULE_HISTORY.addHistoryRun(this.moduleId);
+        this.run();
+    };
+
+    DYNAMIC_MODULES.push(module);
+}
